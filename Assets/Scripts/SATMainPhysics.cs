2462caa9c829d1b4f052fc7b07a96202c7847c22
txeptsyip <lekoro@hotmail.co.uk>
2021-11-22 02:03:45 +0000

    aaaa

--- Assets/Scripts/SATMainPhysics.cs
+++ Assets/Scripts/SATMainPhysics.cs
@@ -34,9 +34,9 @@ public class SATObj
     public Vector3 position;
     public Vector3 Velocity;
     public float Drag;
-    public List<Vector2> vertices = new List<Vector2>();
+    public List<Vector3> vertices = new List<Vector3>();
     public float width;
     public float height;
-    public List<Vector2> normals = new List<Vector2>();
+    public List<Vector3> normals = new List<Vector3>();
 
 }
--- Assets/Scripts/SATPhysObj.cs
+++ Assets/Scripts/SATPhysObj.cs
@@ -7,8 +7,9 @@ public class SATPhysObj : MonoBehaviour
     public SATObj satobj;
     public bool isrect; // check this if shape is rect, to add thing to calculate corners when true
                         // Start is called before the first frame update
-    public List<Vector2> vertices = new List<Vector2>();
-    public List<Vector2> normals = new List<Vector2>();
+    public List<Vector3> vertices = new List<Vector3>();
+    public List<Vector3> normals = new List<Vector3>();
+    public List<Vector3> wVertices = new List<Vector3>();
     void Start()
     {
         Debug.Log("start started");
@@ -18,19 +19,14 @@ public class SATPhysObj : MonoBehaviour
         {
             satobj.width = transform.localScale.x;
             satobj.height = transform.localScale.y;
-            vertices.Add(new Vector2(-satobj.width * 0.5f, satobj.height * 0.5f));
-            vertices.Add(new Vector2(satobj.width * 0.5f, satobj.height * 0.5f));
-            vertices.Add(new Vector2(satobj.width * 0.5f, -satobj.height * 0.5f));
-            vertices.Add(new Vector2(-satobj.width * 0.5f, -satobj.height * 0.5f));
+            vertices.Add(new Vector3(-satobj.width * 0.5f, satobj.height * 0.5f));
+            vertices.Add(new Vector3(satobj.width * 0.5f, satobj.height * 0.5f));
+            vertices.Add(new Vector3(satobj.width * 0.5f, -satobj.height * 0.5f));
+            vertices.Add(new Vector3(-satobj.width * 0.5f, -satobj.height * 0.5f));
             
 
         }
-        for (int a = 0; a < vertices.Count; a++)
-        {
-            normals.Add(vertices[(a + 1) % vertices.Count] - vertices[a]);
-            normals[a] = new Vector2(normals[a].x * -1, normals[a].y);
-            normals[a] = normals[a].normalized;
-        }
+        wVertices.AddRange(vertices);
     }
 
 
@@ -38,7 +34,17 @@ public class SATPhysObj : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        for (int b = 0; b < vertices.Count; b++)
+        {
+            wVertices[b] = vertices[b] + transform.position;
+        }    
+        normals.Clear();
+        for (int a = 0; a < vertices.Count; a++)
+        {
+            normals.Add(wVertices[(a + 1) % wVertices.Count] - wVertices[a]);
+            normals[a] = new Vector3(normals[a].x * -1, normals[a].y);
+            normals[a] = normals[a].normalized;
+        }
     }
 
     void OnDestroy()
